4a59dfac255b3ab54ddbc04f83979c5edab7332e
Stanislav <pnfstas@gmail.com>
2026-08-14 00:31:00 +0300

    14.08.2026T00:30

--- iosApp/iosApp/Views/ContentView.swift
+++ iosApp/iosApp/Views/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
-import WebKit
+import shared
+
+enum AppScreens : Hashable, CaseIterable {
+    case main
+    case catalog
+    case favorites
+    case trash
+    case about
+    case contacts
+    case faq
+}
+
 
 struct ContentView: View {
+    @State private var navigationPath : [AppScreens] = []
+    @State private var toolbarIcons : [String : UIImage?] = ["mercury.png" : nil, "catalog.svg" : nil, "search.svg" : nil, "favorites.svg" : nil, "trash.svg" : nil, "menu.svg" : nil ]
     var body: some View {
         NavigationStack {
-            
+            MainView()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(uiImage: toolbarIcons["mercury.png"]! ?? UIImage())
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                }
+            }
+
         }
-        .navigationBarTitle("Text")
-        .toolbar {
-            ToolbarItem {
+        .navigationDestination(for: AppScreens.self) { screen in
+            switch screen {
+            case .main: MainView()
+            case .catalog:
+            case .favorites:
+            case .trash:
+            case .about:
+            case .contacts:
+            case .faq:
+            }
+        }
+        .task {
+            for key in toolbarIcons.keys {
+                toolbarIcons[key] = await ResourceLoaderKt.loadSharedDrawable(key)
             }
         }
         Grid(horizontalSpacing : 10, verticalSpacing : 10) {
---
+++ iosApp/iosApp/Views/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  MainView.swift
+//  iosApp
+//
+//  Created by Panferov Stanislav on 13.08.2026.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Главная")
+        }
+        .navigationTitle("Главная")
+    }
+}
